1f802681f0cc98a2462190db7906bb2ad1aa73df
Rick Companje <rick@companje.nl>
2016-01-03 21:03:48 +0100

    added Arduino code

---
+++ arduino/tijdmachine/tijdmachine.ino
@@ -0,0 +1,28 @@
+int avg0,avg1;
+float s = 0.8;
+
+void setup() {
+  Serial.begin(9600);
+    avg0=analogRead(0);
+    avg1=analogRead(1);
+//  analogReference(INTERNAL);
+}
+
+void loop() {
+ 
+//  int count=15;
+  //for (int i=0; i<count; i++) {
+    avg0 = (int)round(s*avg0 + (1-s)*analogRead(0));
+    avg1 = (int)round(s*avg1 + (1-s)*analogRead(1));
+  
+  //avg0/=count;
+  //avg1/=count;
+    
+  while (Serial.available()) {
+    Serial.read();
+    Serial.print(avg0);
+    Serial.print(",");
+    Serial.print(avg1);
+    Serial.println();
+  }
+}
